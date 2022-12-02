81a04265612df5a36c953568c11cadf793084df7
Gustavo Cantador <grgcantador@gmail.com>
2022-12-02 08:15:07 -0300

    maybe_final_commit

--- calculadora.py
+++ calculadora.py
@@ -1,32 +1,34 @@
-from math import exp, log10, sin, cos, tan, factorial,degrees
+from math import exp, log10, sin, cos, tan, factorial, degrees, sqrt, log10
+
+
 class calculadora():
 
-
-    def __init__(self): #CONSTRUTOR INICIA AS OPERAÇÕES 
-        resultado = 0    
+    def __init__(self):  # CONSTRUTOR INICIA AS OPERAÇÕES
+        resultado = 0
         print("BEM VINDOA CALCULADORA\n*******************************")
-        self.listar_operacoes() #METODO QUE LISTA AS OPERAÇÕES, FORA DO WHILE PARA INICIALIZAR A PRIMEIRA VEZ
-        while True: 
-            try: #PARA O CODIGO NAO QUEBRAR POR ERRO DE INPUT DO USUARIO
-                resultado = self.rodar(resultado)  #VARIAVEL RESULTADO INICIADA COM 0 A CIMA, MAS DEPOIS RECEBE DO LOOPING IFINITO
+        self.listar_operacoes()  # METODO QUE LISTA AS OPERAÇÕES, FORA DO WHILE PARA INICIALIZAR A PRIMEIRA VEZ
+        while True:
+            try:  # PARA O CODIGO NAO QUEBRAR POR ERRO DE INPUT DO USUARIO
+                resultado = self.rodar(
+                    resultado)  # VARIAVEL RESULTADO INICIADA COM 0 A CIMA, MAS DEPOIS RECEBE DO LOOPING IFINITO
                 if resultado == "break":
                     break
-                print('%.2f' % resultado)
-                
-                if resultado == "listar":
-                    self.listar_operacoes() #METODO PARA LISTAR OPERAÇÕES CASO DESEJADO PELO USUARIO
 
-            except: #INFORMA O USUARIO QUE ELE REALIZOU UM INPUT ERRADO
+                if resultado == "listar":
+                    self.listar_operacoes()  # METODO PARA LISTAR OPERAÇÕES CASO DESEJADO PELO USUARIO
+                else:
+                    print('%.2f' % resultado)
+            except:  # INFORMA O USUARIO QUE ELE REALIZOU UM INPUT ERRADO
                 print("Houve algum erro de input, verifique as instruções")
                 pass
 
-    def prin_finan(): # 'PRINT' DEDICADO PARA CALCULADORA FINANCEIRA
+    def prin_finan():  # 'PRINT' DEDICADO PARA CALCULADORA FINANCEIRA
         x = float(input("Montatnte Inicial (R$): "))
         y = float(input("Taxa de Juros (%): "))
         z = float(input("Periodo (mês): "))
-        return(x,y,z)
+        return (x, y, z)
 
-    def listar_operacoes(self): #METODO QUE LISTA OPERAÇÕES
+    def listar_operacoes(self):  # METODO QUE LISTA OPERAÇÕES
         print("1 - somar")
         print("2 - subtrair")
         print("3 - multiplicar")
@@ -35,32 +37,32 @@ class calculadora():
         print("6 - raiz quadrada")
         print("7 - elevar a x")
         print("8 - raiz x")
-        print("9 - log")
+        print("9 - log base 10")
         print("10 - fatorial")
         print("11 - sen")
         print("12 - cos")
         print("13 - tg")
         print("14 - juros simples")
         print("15 - juros compostos")
-        
-    def rodar(self, res): #METODO QUE RODA O CODIGO, CONDIÇÕES DE OPÇÕES DO USUARIO
-        
+
+    def rodar(self, res):  # METODO QUE RODA O CODIGO, CONDIÇÕES DE OPÇÕES DO USUARIO
+
         a = int(input("Escolha sua operação, digite 0 para encerrar ou 45 para listar operações: "))
 
         if a == 45:
-            return "listar"
+            return "listar" # RETORNA LISTAR QUE É USADO NO INIT PARA A CONDIÇÃO DE LISTAR OPERAÇÕES NO LOOPING INFINITO
         if a == 0:
-            return "break"
-        if a == 5:
+            return "break" #RETORNA A STRING BREAK PARA QUE SEJA USADO COMO CONDIÇÃO PARA PARAR A EXECUÇÃO DO CODIGO CASO O USUARIO DIGITE 0
+        if a in [5, 6, 9, 10, 11, 12, 13]: # CONDIÇÃO PARA RECEBER APENAS UM INPUT
             x = float(input("Digite um numero ou r para resultado anterior: "))
-            y = None
+            y = None #Y É NONE POIS PARA ESSAS OPERAÇÕES SE NECESSITA APENAS UM INPUT
             if x == "r":
                 x = res
 
-        if a == 14 or a == 15:
-            x,y,z = calculadora.prin_finan() 
+        if a in [14, 15]: # CONDIÇÃO PARA EXIBIR A CALCULADORA FINANCEIRA
+            x, y, z = calculadora.prin_finan()
 
-        else: 
+        if a in [1, 2, 3, 4, 7, 8]: #CONDIÇÃO PARA RECEBER 2 INPUTS
             x = input("Digite um numero ou r para resultado anterior: ")
             if x == "r":
                 x = res
@@ -73,7 +75,7 @@ class calculadora():
             else:
                 y = float(y)
 
-        #-------------Simples----------------#
+        # -------------Simples----------------#
 
         if a == 1:
             c = calculadora.simples(x, y)
@@ -90,7 +92,7 @@ class calculadora():
         if a == 4:
             c = calculadora.simples(x, y)
             resultado = c.dividir()
-        #-------------Cientifica----------------#
+        # -------------Cientifica----------------#
         if a == 5:
             c = calculadora.cientifica(x, y)
             resultado = c.elevado_quadrado()
@@ -125,88 +127,89 @@ class calculadora():
 
         if a == 13:
             c = calculadora.cientifica(x, y)
-            resultado = c.tg()       
+            resultado = c.tg()
 
-        #-------------Financeira---------------#
+            # -------------Financeira---------------#
         if a == 14:
-            c = calculadora.Financeira(x,y,z)
+            c = calculadora.Financeira(x, y, z)
             resultado = c.Juros_Simples()
 
         if a == 15:
-            c = calculadora.Financeira(x,y,z)
+            c = calculadora.Financeira(x, y, z)
             resultado = c.Juros_Composto()
 
+        c = None #destrutor
         return resultado
 
-    class simples(): #SUBCLASSE DA CALCULADORA SIMPLES, REALIZA OPERAÇÕES SIMPLES
-        def __init__(self, a, b): #CONSTRUTOR RECEBE OS PARAMETROS
+
+    class simples():  # SUBCLASSE DA CALCULADORA SIMPLES, REALIZA OPERAÇÕES SIMPLES
+        def __init__(self, a, b):  # CONSTRUTOR RECEBE OS PARAMETROS
             self.a = a
             self.b = b
 
         def somar(self):
-            return self.a+self.b
-        
+            return self.a + self.b
+
         def subtrair(self):
-            return self.a-self.b
-        
+            return self.a - self.b
+
         def multiplicar(self):
-            return self.a*self.b
+            return self.a * self.b
 
         def dividir(self):
             while self.b == 0:
                 print("Nao existe divisao por 0, tente novamente")
                 self.a = float(input("Digite um numero: "))
                 self.b = float(input("Digite um numero: "))
-            return self.a/self.b
+            return self.a / self.b
 
-    class cientifica():
-    
-        def __init__(self, a, b=0): #CONSTRUTOR RECEBE OS PARAMETROS
+    class cientifica(): #SUBCLASSE CALCULADORA CIENTIFICA
+
+        def __init__(self, a, b):  # CONSTRUTOR RECEBE OS PARAMETROS
             self.a = a
             self.b = b
 
         def elevado_quadrado(self):
             # return self.a*self.a
-            c = calculadora.simples(self.a, self.a) #
+            c = calculadora.simples(self.a, self.a)  #
             return c.multiplicar()
-        
-        def raiz_quadrada(self): 
-            pass
 
-        def elevar_a_x(self): 
-            pass
+        def raiz_quadrada(self):
+            return sqrt(self.a)
 
-        def raiz_x(self): 
-            pass
+        def elevar_a_x(self):
+            return self.a**self.b
 
-        def log(self): 
-            pass
+        def raiz_x(self):
+            return self.a**(1/self.b)
+        def log(self):
+            return log10(self.a)
 
-        def fatorial(self): 
-            pass
+        def fatorial(self):
+            return factorial(self.a)
 
-        def sen(self): 
-            return (sin(self.a)) # Convertendo para graus
+        def sen(self):
+            return (sin(self.a))  # Convertendo para graus
 
-        def cos(self): 
-            return (cos(self.a)) # Convertendo para graus
-        
-        def tg(self): 
-            return (tan(self.a)) # Convertendo para graus
+        def cos(self):
+            return (cos(self.a))  # Convertendo para graus
 
-    class Financeira():
-        def __init__(self, P, I, N):
-            #self.M = M # Montante
-            self.P = P # Montante Inicial (R$)
-            self.I = I # Taxa de Juros (%)
-            self.N = N # Periodo (mês)
+        def tg(self):
+            return (tan(self.a))  # Convertendo para graus
+
+    class Financeira(): #SUBCLASSE CALCULADORA FINANCEIRA
+        def __init__(self, P, I, N): #CONSTRUTOR RECEBE PARAMETROS
+            self.P = P  # Montante Inicial (R$)
+            self.I = I  # Taxa de Juros (%)
+            self.N = N  # Periodo (mês)
 
         def Juros_Simples(self):
-            J_S = (self.P + (self.P * (self.I/100) * self.N))
+            J_S = (self.P + (self.P * (self.I / 100) * self.N))
             return J_S
 
         def Juros_Composto(self):
-            J_C = self.P * ((1 + (self.I/100))**self.N)
+            J_C = self.P * ((1 + (self.I / 100)) ** self.N)
             return J_C
 
-calculadora()     
+
+calculadora()
